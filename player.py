a17c42de6f3f2b30d0547255eaa67b958b23e4da
ArwenK <64578473+ArwenK@users.noreply.github.com>
2020-05-15 11:46:37 +0200

    Update player.py

--- player.py
+++ player.py
@@ -69,9 +69,9 @@ class Player:
         
         # Enregistrement du prix 
         if (time == 0):
-            self.memoire_prix_interne[47] = self.prices.sale[47]
+            self.memoire_prix_interne[47] = self.prices['sale'][47]
         else:
-            self.memoire_prix_interne[t-1] = self.prices.sale[t-1]
+            self.memoire_prix_interne[t-1] = self.prices['sale'][t-1]
         
         # On vérifie qu'on ne dépasse pas la puissance max.
         if (abs(chargement_batterie) > self.max_load):
